e163fb546411fda0691bf0ad1f39d9d46eac2aa6
aindrila <aindrila03@gmail.com>
2023-08-15 12:31:58 +0530

    we are committing the helloworld.cpp

---
+++ helloworld.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+class print{
+
+   public:
+       void pri()
+       {
+         cout<<"print helloworld";
+       } 
+
+};
+int main()
+{
+    print obj1;
+    obj1.pri();
+}
